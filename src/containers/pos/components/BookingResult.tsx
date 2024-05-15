5e812de6ce21c34be64f2a82a7f3c6f76ceb7e34
Bindholt <cbindholt@gmail.com>
2024-05-15 22:07:06 +0200

    added button styling

--- src/containers/pos/components/BookingResult.tsx
+++ src/containers/pos/components/BookingResult.tsx
@@ -1,12 +1,22 @@
 import type { IBowlingBooking, IAirHockeyBooking, IDinnerBooking } from '../../../types/booking';
 import translateBookingKey from '../helpers/translateBookingKey';
 
+function BookingButtons() {
+    return (
+        <div className="flex flex-col justify-end w-2/5 pl-5 items-end">
+            <input type="button" value="Cancel" className='bg-red-500 border-zinc-500 border cursor-pointer text-black text-center py-2 px-4 my-3.5 mr-6 rounded-md hover:bg-zinc-50 w-2/5 '/>
+        </div>
+    )
+}
+
 function BookingLine({bookingLine}: {bookingLine: { [key: string]: number | string }}) {
+    const lineTitle = Object.keys(bookingLine)[0];
+    const lineValue = Object.values(bookingLine)[0];
     return (
         <div className="flex mb-2">
-            <div className="flex bg-white border border-zinc-400 rounded-lg w-3/5 p-2.5 m-2.5">
-                <div className="w-1/2 pr-2">{translateBookingKey(Object.keys(bookingLine)[0])}:</div>
-                <div className="w-1/2 pl-2 text-left">{Object.values(bookingLine)[0]}</div>
+            <div className="flex bg-white border border-zinc-400 rounded-lg min-w-full p-2.5 m-2.5">
+                <div className="w-1/2 ">{translateBookingKey(lineTitle)}:</div>
+                <div className="w-1/2 text-left">{lineValue}</div>
             </div>
         </div>
     )
@@ -16,13 +26,17 @@ function BookingResult({bookings}: {bookings: ( IBowlingBooking | IAirHockeyBook
     return (
     <>
         {bookings.map((booking, index) => (
-            <div key={index}  className="flex flex-col border border-zinc-400 rounded-md min-w-96 w-2/6 bg-zinc-100 m-5">
+            <div key={index} className="flex flex-row border border-zinc-400 rounded-md min-w-96 w-2/6 bg-zinc-100 m-5">
+            <div className="flex flex-col w-3/5">
                 {
                     Object.entries(booking).map(([key, value]) => (
                         <BookingLine key={key} bookingLine={{[key]: value}}/>
                     ))
                 }
             </div>
+            <BookingButtons/>
+        </div>
+            
         ))}
     </>
     )
