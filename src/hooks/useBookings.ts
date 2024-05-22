67c8e449b1a61bb5ee3481591e788c44854bcfcc
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-05-22 02:36:50 +0200

    #4 Major refactoring: removed all unused functions and rewrote most functions from 1 that guides towards 3 specified functions for specific fetches, into more generic functions

--- src/hooks/useBookings.ts
+++ src/hooks/useBookings.ts
@@ -1,284 +1,120 @@
-import {useState} from 'react';
+import {useInfiniteQuery, useQuery} from '@tanstack/react-query';
 import type {IAirHockeyBooking, IBowlingBooking, IDinnerBooking, IPatchBooking} from '../types/booking';
 import toast from 'react-hot-toast';
 
-function useBookings() {
-    const [bowlingBookings, setBowlingBookings] = useState<IBowlingBooking[]>([]);
-    const [airHockeyBookings, setAirHockeyBookings] = useState<IAirHockeyBooking[]>([]);
-    const [dinnerBookings, setDinnerBookings] = useState<IDinnerBooking[]>([]);
-    const url = import.meta.env.VITE_API_URL;
-
-    async function getAll() {
-        await getAllBowlingBookings();
-        await getAllAirHockeyBookings();
-        await getAllDinnerBookings();
-    }
-
-    async function getAllById(id: number) {
-        const bookings: (IBowlingBooking | IAirHockeyBooking | IDinnerBooking)[] = [];
-        const bowlingBoookings: IBowlingBooking = await getBowlingBookingById(id);
-        const airHockeyBookings: IAirHockeyBooking = await getAirHockeyBookingById(id);
-        const dinnerBookings: IDinnerBooking = await getDinnerBookingById(id);
-
-        if (bowlingBoookings) bookings.push(bowlingBoookings);
-        if (airHockeyBookings) bookings.push(airHockeyBookings);
-        if (dinnerBookings) bookings.push(dinnerBookings);
-        return bookings;
-    }
-
-    async function getAllByEmail(email: string) {
-        const bookings: (IBowlingBooking | IAirHockeyBooking | IDinnerBooking)[] = [];
-        const bowlingBoookings: IBowlingBooking[] = await getBowlingBookingsByEmail(email);
-        const airHockeyBookings: IAirHockeyBooking[] = await getAirHockeyBookingsByEmail(email);
-        const dinnerBookings: IDinnerBooking[] = await getDinnerBookingsByEmail(email);
-
-        if (bowlingBoookings) bookings.push(...bowlingBoookings);
-        if (airHockeyBookings) bookings.push(...airHockeyBookings);
-        if (dinnerBookings) bookings.push(...dinnerBookings);
-        return bookings;
-    }
-
-    async function getAllBowlingBookings() {
-        try {
-            const response = await fetch(`${url}/bowling`);
-            const data = await response.json();
-            setBowlingBookings(data);
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    async function getBowlingBookingById(id: number) {
-        try {
-            const response = await fetch(`${url}/bowling/${id}`);
-            if (response.status === 404) {
-                return null;
-            }
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            console.error(error);
-            return [];
-        }
-    }
-
-    async function getBowlingBookingsByEmail(email: string) {
-        try {
-            const response = await fetch(`${url}/bowling/email/${email}`);
-            const data = await response.json();
-            if (response.status === 404) {
-                return null;
-            }
-            return data;
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    async function getAllAirHockeyBookings() {
-        try {
-            const response = await fetch(`${url}/airhockey`);
-            const data = await response.json();
-            setAirHockeyBookings(data);
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    async function getAirHockeyBookingById(id: number) {
-        try {
-            const response = await fetch(`${url}/airhockey/${id}`);
-            if (response.status === 404) {
-                return null;
-            }
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    async function getAirHockeyBookingsByEmail(email: string) {
-        try {
-            const response = await fetch(`${url}/airhockey/email/${email}`);
-            const data = await response.json();
-            if (response.status === 404) {
-                return null;
-            }
-            return data;
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    async function getAllDinnerBookings() {
-        try {
-            const response = await fetch(`${url}/dinner`);
-            const data = await response.json();
-            setDinnerBookings(data);
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    async function getDinnerBookingById(id: number) {
-        try {
-            const response = await fetch(`${url}/dinner/${id}`);
-            if (response.status === 404) {
-                return null;
-            }
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    async function getDinnerBookingsByEmail(email: string) {
-        try {
-            const response = await fetch(`${url}/dinner/email/${email}`);
-            const data = await response.json();
-            if (response.status === 404) {
-                return null;
-            }
-            return data;
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    async function update(id: number, booking: IPatchBooking, type: string) {
-        switch (type) {
-            case "bowling":
-                return updateBowling(id, booking);
-            case "airhockey":
-                return updateAirHockey(id, booking);
-            case "dinner":
-                return updateDinner(id, booking);
-            default:
-                return null;
-        }
-    }
-
-    async function updateBowling(id: number, booking: IPatchBooking) {
-        try {
-            const response = await fetch(`${url}/bowling/${id}`, {
-                method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(booking)
-            });
-            const data = await response.json();
-            if(response.status === 200) {
-                toast.success("Booking updated");
-                return data;
-            } else {
-                toast.error("Booking could not be updated");
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    async function updateAirHockey(id: number, booking: IPatchBooking) {
-        try {
-            const response = await fetch(`${url}/airhockey/${id}`, {
-                method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(booking)
-            });
-            const data = await response.json();
-            if(response.status === 200) {
-                toast.success("Booking updated");
-                return data;
-            } else {
-                toast.error("Booking could not be updated");
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    async function updateDinner(id: number, booking: IPatchBooking) {
-        try {
-            const response = await fetch(`${url}/dinner/${id}`, {
-                method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(booking)
-            });
-            const data = await response.json();
-            if(response.status === 200) {
-                toast.success("Booking updated");
-                return data;
-            } else {
-                toast.error("Booking could not be updated");
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-        async function checkAvailability(start: string, end: string, type: string, numberOfGuests?: number) {
-            console.log('Checking availability in useBookings')
-            console.log(`type is ${type}`)
-            switch (type) {
-                case "bowling":
-                    return checkBowlingAvailability(start, end);
-                case "airHockey":
-                    return checkAirHockeyAvailability(start, end);
-                case "dinner":
-                    return checkDinnerAvailability(start, end, numberOfGuests!);
-                default:
-                    return null;
-            }
-        }
-
-        async function checkBowlingAvailability(start: string, end: string) {
-            try {
-                console.log('Checking bowling availability')
-                const response = await fetch(`${url}/bowling-lanes/availability?start=${start}&end=${end}`);
-                return await response.json();
-            } catch (error) {
-                console.error(error);
-            }
-        }
-
-        async function checkAirHockeyAvailability(start: string, end: string) {
-            try {
-                console.log('Checking air hockey availability')
-                const response = await fetch(`${url}/airhockey-tables/availability?start=${start}&end=${end}`);
-                return await response.json();
-            } catch (error) {
-                console.error(error);
-            }
-        }
-
-        async function checkDinnerAvailability(start: string, end: string, numberOfGuests: number) {
-            try {
-                console.log('Checking dinner availability')
-                const response = await fetch(`${url}/restaurants/availability?start=${start}&end=${end}&numberOfGuests=${numberOfGuests}`);
-                return await response.json();
-            } catch (error) {
-                console.error(error);
-            }
-        }
-
-    return {
-        bowlingBookings,
-        airHockeyBookings,
-        dinnerBookings,
-        getAll,
-        getAllById,
-        getAllByEmail,
-        update,
-        checkAvailability
-    }
+interface BookingResponse<T> {
+    data: T[];
+    nextPage?: number;
 }
 
+function useBookings() {
+    const url = import.meta.env.VITE_API_URL;
+
+    const getBookingsByEmail = async <T>(type: string, email: string, pageParam: number, size: number): Promise<BookingResponse<T>> => {
+    try {
+        const response = await fetch(`${url}/${type}/email/${email}?page=${pageParam}&size=${size}`);
+        const data = await response.json();
+        if (response.status === 404) {
+            return {data: []};
+        }
+        // Extract the content field from the data
+        return {data: data.content, nextPage: data.content.length === size ? pageParam + 1 : undefined};
+    } catch (error) {
+        console.error(error);
+        return {data: [], nextPage: undefined};
+    }
+};
+
+    const useGetAllByEmail = (email: string, size: number = 10) => {
+        console.log('useGetAllByEmail', email, size);
+        const response = useInfiniteQuery<BookingResponse<IBowlingBooking | IAirHockeyBooking | IDinnerBooking>, Error>({
+            queryKey: ['bookings', email],
+            queryFn: async ({pageParam = 0}) => {
+                const bookingTypes = ['bowling', 'airhockey', 'dinner'];
+                const results = await Promise.all(
+                    bookingTypes.map(type => getBookingsByEmail<IBowlingBooking | IAirHockeyBooking | IDinnerBooking>(type, email, pageParam as number, size))
+                );
+
+                return results.reduce((acc, result) => {
+                    acc.data.push(...result.data);
+                    acc.nextPage = result.data.length === size ? pageParam as number + 1 : acc.nextPage;
+                    return acc;
+                }, {data: [], nextPage: results[0].data.length});
+            },
+            getNextPageParam: (lastPage) => lastPage.nextPage,
+            initialPageParam: 0,
+        });
+        // log the number of elements in the pages array
+        console.log('response data', response);
+        return response;
+    };
+
+    const getBookingById = async (id: number, type: string): Promise<IBowlingBooking | IAirHockeyBooking | IDinnerBooking | null> => {
+        try {
+            const response = await fetch(`${url}/${type}/${id}`);
+            if (response.status === 404) {
+                return null;
+            }
+            return await response.json();
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
+    };
+
+    const useGetBookingById = (id: number) => {
+        const bookingTypes = ['bowling', 'airhockey', 'dinner'];
+
+        const results = bookingTypes.map(type =>
+            useQuery({
+                queryKey: ['booking', id, type],
+                queryFn: () => getBookingById(id, type),
+                enabled: !!id,
+            })
+        );
+
+        return results
+            .map(result => result.data)
+            .filter(data => data !== null) as (IBowlingBooking | IAirHockeyBooking | IDinnerBooking)[];
+    };
+
+    async function update(id: number, booking: IPatchBooking, type: string) {
+        try {
+            const response = await fetch(`${url}/${type}/${id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(booking)
+            });
+            const data = await response.json();
+            if (response.status === 200) {
+                toast.success("Booking updated");
+                return data;
+            } else {
+                toast.error("Booking could not be updated");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function checkAvailability(start: string, end: string, type: string, numberOfGuests?: number) {
+        const urlPath = type === 'dinner' ? `${type}/availability?start=${start}&end=${end}&numberOfGuests=${numberOfGuests}` : `${type}-lanes/availability?start=${start}&end=${end}`;
+        try {
+            const response = await fetch(`${url}/${urlPath}`);
+            return await response.json();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return {
+        useGetAllByEmail,
+        useGetBookingById,
+        update,
+        checkAvailability
+    };
+}
 
 export default useBookings;
