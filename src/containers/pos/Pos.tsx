e133992e07110b1faf84238ff80150ddcc975f7b
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-05-22 02:44:55 +0200

    #4 implemented react query infinity scrolling when fetching booking by customer email

--- src/containers/pos/Pos.tsx
+++ src/containers/pos/Pos.tsx
@@ -1,16 +1,15 @@
-import BookingResult from "./components/BookingResult";
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import useBookings from '../../hooks/useBookings';
-import type { IBowlingBooking, IAirHockeyBooking, IDinnerBooking } from '../../types/booking';
 import { Dispatch, SetStateAction } from 'react';
 import PageLayout from "../../components/PageLayout";
+import BookingResult from "./components/BookingResult";
 
 interface SearchBarProps {
     setBookingId: Dispatch<SetStateAction<string>>;
     setBookingEmail: Dispatch<SetStateAction<string>>;
 }
 
-function SearchBar({setBookingId, setBookingEmail}: SearchBarProps){
+function SearchBar({ setBookingId, setBookingEmail }: SearchBarProps) {
     const [searchId, setSearchId] = useState("");
     const [searchEmail, setSearchEmail] = useState("");
 
@@ -20,46 +19,52 @@ function SearchBar({setBookingId, setBookingEmail}: SearchBarProps){
         }
         if (searchEmail) {
             setBookingEmail(searchEmail);
-        } 
+        }
     }
 
     return (
         <div className="flex flex-col border border-zinc-400 rounded-md min-w-96 w-2/6 bg-zinc-100 m-5">
-            <input type="text" placeholder="Booking ID" onChange={(e) => {setSearchId(e.target.value)}} className="bg-white border border-zinc-400 rounded-lg w-3/5 p-2.5 m-2.5"/>
+            <input type="text" placeholder="Booking ID" onChange={(e) => { setSearchId(e.target.value) }} className="bg-white border border-zinc-400 rounded-lg w-3/5 p-2.5 m-2.5" />
             <div className="flex justify-between">
-                <input type="text" placeholder="E-mail" onChange={(e) => {setSearchEmail(e.target.value)}} className="bg-white border border-zinc-400 rounded-lg w-3/5 p-2.5 m-2.5"/>
-                <input type="button" value="Search" onClick={handleSearch} className="bg-zinc-300 border-zinc-500 border cursor-pointer text-gray-600 py-2 px-4 my-2.5 mr-6 rounded-md hover:bg-zinc-50 "/>
+                <input type="text" placeholder="E-mail" onChange={(e) => { setSearchEmail(e.target.value) }} className="bg-white border border-zinc-400 rounded-lg w-3/5 p-2.5 m-2.5" />
+                <input type="button" value="Search" onClick={handleSearch} className="bg-zinc-300 border-zinc-500 border cursor-pointer text-gray-600 py-2 px-4 my-2.5 mr-6 rounded-md hover:bg-zinc-50 " />
             </div>
         </div>
-    )
+    );
 }
 
 function Pos() {
-    const { getAllById, getAllByEmail } = useBookings();
-    const [bookings, setBookings] = useState<(IBowlingBooking | IAirHockeyBooking | IDinnerBooking)[]>([]);
-    const [bookingId, setBookingId] = useState("");
-    const [bookingEmail, setBookingEmail] = useState("");
+    const { useGetAllByEmail, useGetBookingById } = useBookings();
+    const [bookingId, setBookingId] = useState<string>("");
+    const [bookingEmail, setBookingEmail] = useState<string>("");
 
-    useEffect(() => {
-        (async () => {
-            if (bookingId) {
-                const responseBookings = await getAllById(Number(bookingId));
-                setBookings(responseBookings);
-            } else if (bookingEmail) {
-                const responseBookings = await getAllByEmail(bookingEmail);
-                setBookings(responseBookings);
-            }
-        })();
-    }, [bookingId, bookingEmail]);
+    const {
+        data: emailData,
+        fetchNextPage,
+        hasNextPage,
+        isFetchingNextPage,
+    } = useGetAllByEmail(bookingEmail);
+
+    const bookingIdNumber = Number(bookingId);
+    const bookingDataById = useGetBookingById(bookingIdNumber);
+
+    const bookingData = bookingIdNumber
+        ? bookingDataById
+        : emailData?.pages.flatMap((page: any) => page.data) || [];
 
     return (
         <PageLayout>
-            <div className="h-1"></div> {/*jeg mister forstanden, jeg kan ik rykke SearchBar nedaf uden at tilføje et element over den. Margin skærer ind i nav bar :') todo:fix lol*/}
+            <div className="h-1"></div>
             <SearchBar setBookingId={setBookingId} setBookingEmail={setBookingEmail} />
-            {!!bookings.length && <BookingResult bookings={bookings} setBookings={setBookings} />}
-            
+                <BookingResult
+                    bookings={bookingData}
+                    setBookings={() => {}}
+                    fetchNextPage={fetchNextPage}
+                    hasNextPage={hasNextPage || false}
+                    isFetchingNextPage={isFetchingNextPage}
+                />
         </PageLayout>
-    )
+    );
 }
 
 export default Pos;
--- src/containers/pos/components/BookingResult.tsx
+++ src/containers/pos/components/BookingResult.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { IBowlingBooking, IAirHockeyBooking, IDinnerBooking } from '../../../types/booking';
+import React, {useEffect, useRef, useState} from 'react';
+import {IBowlingBooking, IAirHockeyBooking, IDinnerBooking} from '../../../types/booking';
 import useBookings from '../../../hooks/useBookings';
 import BookingLine from './BookingLine';
 import BookingButtons from './BookingButtons';
@@ -7,6 +7,9 @@ import BookingButtons from './BookingButtons';
 interface BookingResultProps {
     bookings: (IBowlingBooking | IAirHockeyBooking | IDinnerBooking)[];
     setBookings: React.Dispatch<React.SetStateAction<(IBowlingBooking | IAirHockeyBooking | IDinnerBooking)[]>>;
+    fetchNextPage: () => void;
+    hasNextPage: boolean;
+    isFetchingNextPage: boolean;
 }
 
 type OriginalBooking = IBowlingBooking | IAirHockeyBooking | IDinnerBooking | undefined;
@@ -14,23 +17,47 @@ type EditedBooking = {
     [key: string]: any;
 } & Partial<IBowlingBooking & IAirHockeyBooking & IDinnerBooking>;
 
-function BookingResult({ bookings, setBookings }: BookingResultProps) {
-    const { update, checkAvailability } = useBookings();
+function BookingResult({bookings, setBookings, fetchNextPage, hasNextPage, isFetchingNextPage}: BookingResultProps) {
+    const {update, checkAvailability} = useBookings();
     const [editMode, setEditMode] = useState<{ [key: number]: boolean }>({});
     const [editedBookings, setEditedBookings] = useState<{ [key: number]: EditedBooking }>({});
     const [availabilityStatus, setAvailabilityStatus] = useState<{ [key: number]: boolean | null }>({});
 
+    const observerRef = useRef<IntersectionObserver | null>(null);
+    const loadMoreRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        if (observerRef.current) observerRef.current.disconnect();
+
+        observerRef.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting && hasNextPage && !isFetchingNextPage) {
+                fetchNextPage();
+            }
+        });
+
+        if (loadMoreRef.current) {
+            observerRef.current.observe(loadMoreRef.current);
+        }
+
+        return () => {
+            if (observerRef.current) observerRef.current.disconnect();
+        };
+    }, [hasNextPage, fetchNextPage, isFetchingNextPage]);
+
     function onCancel(id: number, bookingType: string) {
-        update(id, { status: 'CANCELLED' }, bookingType);
-        setBookings((prev) => prev.map((booking) => booking.id === id ? { ...booking, status: 'CANCELLED' } : booking));
+        update(id, {status: 'CANCELLED'}, bookingType);
+        setBookings((prev) => prev.map((booking) => booking && booking.id === id ? {
+            ...booking,
+            status: 'CANCELLED'
+        } : booking));
     }
 
     function onEditToggle(id: number) {
-        setEditMode((prev) => ({ ...prev, [id]: !prev[id] }));
+        setEditMode((prev) => ({...prev, [id]: !prev[id]}));
     }
 
     function onAccept(id: number, bookingType: string) {
-        const originalBooking: OriginalBooking = bookings.find((booking) => booking.id === id);
+        const originalBooking: OriginalBooking = bookings.find((booking) => booking && booking.id === id);
         const editedBooking: EditedBooking = editedBookings[id];
 
         if (!originalBooking || !editedBooking) return;
@@ -49,7 +76,7 @@ function BookingResult({ bookings, setBookings }: BookingResultProps) {
         // If no changes
         if (!changes.date && !changes.start && !changes.end) {
             console.log('No changes');
-            setEditMode((prev) => ({ ...prev, [id]: false }));
+            setEditMode((prev) => ({...prev, [id]: false}));
             return;
         }
 
@@ -63,7 +90,6 @@ function BookingResult({ bookings, setBookings }: BookingResultProps) {
         if (changes.end && !changes.date) editedBooking.start = `${originalBookingDate}T${originalBookingStartTime}`;
 
         // Ensure the changes are applied before checking availability
-        // is this necessary?
         if (editedBooking.start && editedBooking.end) {
             checkAvailability(editedBooking.start, editedBooking.end, bookingType, editedBooking.numberOfGuests).then((isAvailable) => {
                 if (!isAvailable) {
@@ -72,8 +98,8 @@ function BookingResult({ bookings, setBookings }: BookingResultProps) {
                 } else {
                     console.log('Available');
                     update(id, editedBooking, bookingType);
-                    setBookings((prev) => prev.map((booking) => booking.id === id ? { ...booking, ...editedBooking } : booking));
-                    setEditMode((prev) => ({ ...prev, [id]: false }));
+                    setBookings((prev) => prev.map((booking) => booking && booking.id === id ? {...booking, ...editedBooking} : booking));
+                    setEditMode((prev) => ({...prev, [id]: false}));
                 }
             });
         }
@@ -82,18 +108,18 @@ function BookingResult({ bookings, setBookings }: BookingResultProps) {
     function handleFieldChange(id: number, field: string, value: any, bookingType: string) {
         setEditedBookings((prev) => {
             const prevBooking = prev[id] || {};
-            const newBooking = { ...prevBooking };
+            const newBooking = {...prevBooking};
 
             if (field === 'startDate') {
-                const existingStartTime = prevBooking.start ? prevBooking.start.split('T')[1] : (bookings.find(booking => booking.id === id)?.start.split('T')[1] || '00:00:00');
-                const existingEndTime = prevBooking.end ? prevBooking.end.split('T')[1] : (bookings.find(booking => booking.id === id)?.end.split('T')[1] || '00:00:00');
+                const existingStartTime = prevBooking.start ? prevBooking.start.split('T')[1] : (bookings.find(booking => booking && booking.id === id)?.start.split('T')[1] || '00:00:00');
+                const existingEndTime = prevBooking.end ? prevBooking.end.split('T')[1] : (bookings.find(booking => booking && booking.id === id)?.end.split('T')[1] || '00:00:00');
                 newBooking.start = `${value}T${existingStartTime}`;
                 newBooking.end = `${value}T${existingEndTime}`;
             } else if (field === 'start') {
-                const existingDate = prevBooking.start ? prevBooking.start.split('T')[0] : (bookings.find(booking => booking.id === id)?.start.split('T')[0] || '1970-01-01');
+                const existingDate = prevBooking.start ? prevBooking.start.split('T')[0] : (bookings.find(booking => booking && booking.id === id)?.start.split('T')[0] || '1970-01-01');
                 newBooking.start = `${existingDate}T${value}`;
             } else if (field === 'end') {
-                const existingDate = prevBooking.end ? prevBooking.end.split('T')[0] : (bookings.find(booking => booking.id === id)?.end.split('T')[0] || '1970-01-01');
+                const existingDate = prevBooking.end ? prevBooking.end.split('T')[0] : (bookings.find(booking => booking && booking.id === id)?.end.split('T')[0] || '1970-01-01');
                 newBooking.end = `${existingDate}T${value}`;
             } else if (field === 'numberOfGuests') {
                 newBooking[field] = parseInt(value, 10);
@@ -102,15 +128,13 @@ function BookingResult({ bookings, setBookings }: BookingResultProps) {
             }
 
             if (newBooking.start && newBooking.end) {
-
-                if (newBooking.start && newBooking.end) {
-                    if (bookingType === 'dinner' && newBooking.numberOfGuests === undefined) {
-                        const originalBooking: OriginalBooking = bookings.find((booking) => booking.id === id);
-                        if (originalBooking && 'numberOfGuests' in originalBooking) {
-                            newBooking.numberOfGuests = (originalBooking as IDinnerBooking).numberOfGuests;
-                        }
+                if (bookingType === 'dinner' && newBooking.numberOfGuests === undefined) {
+                    const originalBooking: OriginalBooking = bookings.find((booking) => booking && booking.id === id);
+                    if (originalBooking && 'numberOfGuests' in originalBooking) {
+                        newBooking.numberOfGuests = (originalBooking as IDinnerBooking).numberOfGuests;
                     }
                 }
+
                 checkAvailability(newBooking.start, newBooking.end, bookingType, newBooking.numberOfGuests).then((isAvailable) => {
                     setAvailabilityStatus((prevStatus) => ({
                         ...prevStatus,
@@ -129,36 +153,39 @@ function BookingResult({ bookings, setBookings }: BookingResultProps) {
     return (
         <>
             {bookings.map((booking, index) => {
-                const isEditing = editMode[booking.id] || false;
-                const bookingType = booking.hasOwnProperty('laneId') ? 'bowling' : booking.hasOwnProperty('tableId') ? 'airHockey' : 'dinner';
-                return (
-                    <div key={index}
-                         className={`flex flex-row border border-zinc-400 rounded-md min-w-96 w-2/6 bg-zinc-100 m-5 ${isEditing ? 'animate-flip' : ''}`}>
-                        <div className="flex flex-col w-3/5">
-                            {
-                                Object.entries(booking).map(([key, value]) => (
-                                    <BookingLine
-                                        key={key}
-                                        bookingLine={{ [key]: value }}
-                                        editable={isEditing && ['customerEmail', 'date', 'start', 'end', 'status', 'childFriendly'].includes(key)}
-                                        onFieldChange={(field, newValue) => handleFieldChange(booking.id, field, newValue, bookingType)}
-                                        availability={availabilityStatus[booking.id] || null}
-                                    />
-                                ))
-                            }
+                    if (!booking) return null;  // Skip undefined bookings
+                    const isEditing = editMode[booking.id] || false;
+                    const bookingType = booking.hasOwnProperty('laneId') ? 'bowling' : booking.hasOwnProperty('tableId') ? 'airHockey' : 'dinner';
+                    return (
+                        <div key={index}
+                             className={`flex flex-row border border-zinc-400 rounded-md min-w-96 w-2/6 bg-zinc-100 m-5 ${isEditing ? 'animate-flip' : ''}`}>
+                            <div className="flex flex-col w-3/5">
+                                {
+                                    Object.entries(booking).map(([key, value]) => (
+                                        <BookingLine
+                                            key={key}
+                                            bookingLine={{[key]: value}}
+                                            editable={isEditing && ['customerEmail', 'date', 'start', 'end', 'status', 'childFriendly'].includes(key)}
+                                            onFieldChange={(field, newValue) => handleFieldChange(booking.id, field, newValue, bookingType)}
+                                            availability={availabilityStatus[booking.id] || null}
+                                        />
+                                    ))
+                                }
+                            </div>
+                            <BookingButtons
+                                booking={booking}
+                                onCancel={onCancel}
+                                onEditToggle={onEditToggle}
+                                onAccept={onAccept}
+                                isEditing={isEditing}
+                            />
                         </div>
-                        <BookingButtons
-                            booking={booking}
-                            onCancel={onCancel}
-                            onEditToggle={onEditToggle}
-                            onAccept={onAccept}
-                            isEditing={isEditing}
-                        />
-                    </div>
-                );
-            })}
+                    );
+                }
+            )}
+            <div ref={loadMoreRef} style={{height: '20px'}}/>
         </>
-    )
+    );
 }
 
 export default BookingResult;
