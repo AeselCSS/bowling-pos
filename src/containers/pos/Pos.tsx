fd13a168ae0b3a5a33662eb2f2721fff516785c0
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-05-27 15:26:32 +0200

    optimised for better useage of refactored useBookings hook

--- src/containers/pos/Pos.tsx
+++ src/containers/pos/Pos.tsx
@@ -1,13 +1,11 @@
 import BookingResult from "./components/BookingResult";
 import SaleProducts from "./components/SaleProducts";
 import Basket from "./components/Basket";
-import React, { useState, useEffect, Dispatch, SetStateAction } from 'react';
+import { useState, Dispatch, SetStateAction, FormEvent } from 'react';
 import useBookings from '../../hooks/useBookings';
 import useSaleProducts from '../../hooks/useSaleProducts';
-import type { IBowlingBooking, IAirHockeyBooking, IDinnerBooking } from '../../types/booking';
 import PageLayout from "../../components/PageLayout";
 import { IBasketProduct } from "../../types/basketProduct";
-
 
 interface SearchBarProps {
     setBookingId: Dispatch<SetStateAction<string>>;
@@ -18,7 +16,7 @@ function SearchBar({ setBookingId, setBookingEmail }: SearchBarProps) {
     const [searchId, setSearchId] = useState("");
     const [searchEmail, setSearchEmail] = useState("");
 
-    function handleSearch(event: React.FormEvent) {
+    function handleSearch(event: FormEvent) {
         event.preventDefault(); // Prevent the form from refreshing the page
         if (!isNaN(Number(searchId))) {
             setBookingId(searchId);
@@ -29,7 +27,7 @@ function SearchBar({ setBookingId, setBookingEmail }: SearchBarProps) {
     }
 
     return (
-        <form onSubmit={handleSearch} className="flex flex-col border border-zinc-400 rounded-md min-w-96 w-2/6 bg-zinc-100 m-5">
+        <form onSubmit={handleSearch} className="flex flex-col border border-zinc-400 rounded-md min-w-96 bg-zinc-100 ml-5 mt-5">
             <input type="text" placeholder="Booking ID" onChange={(e) => { setSearchId(e.target.value) }} className="bg-white border border-zinc-400 rounded-lg w-3/5 p-2.5 m-2.5" />
             <div className="flex justify-between">
                 <input type="text" placeholder="E-mail" onChange={(e) => { setSearchEmail(e.target.value) }} className="bg-white border border-zinc-400 rounded-lg w-3/5 p-2.5 m-2.5" />
@@ -40,48 +38,42 @@ function SearchBar({ setBookingId, setBookingEmail }: SearchBarProps) {
 }
 
 function Pos() {
-    const { useGetAllByEmail, useGetBookingById } = useBookings();
+    const { fetchBookingsByEmail, fetchBookingById, bookings, setBookings, hasNextPage, isFetchingNextPage, fetchNextPage } = useBookings();
     const { saleProducts, setSaleProducts } = useSaleProducts();
     const [bookingId, setBookingId] = useState<string>("");
     const [bookingEmail, setBookingEmail] = useState<string>("");
     const [basket, setBasket] = useState<(IBasketProduct)[]>([]);
-    const {
-        data: emailData,
-        fetchNextPage,
-        hasNextPage,
-        isFetchingNextPage,
-    } = useGetAllByEmail(bookingEmail);
 
-    const bookingIdNumber = Number(bookingId);
-    const bookingDataById = useGetBookingById(bookingIdNumber);
-
-    const bookingData = bookingIdNumber
-        ? bookingDataById
-        : emailData?.pages.flatMap((page: any) => page.data) || [];
+    fetchBookingsByEmail(bookingEmail);
+    fetchBookingById(Number(bookingId));
 
     return (
         <PageLayout>
-            <div className="h-1"></div>
-            <SearchBar setBookingId={setBookingId} setBookingEmail={setBookingEmail} />
+
+
 
             <div className="flex">
-                <div className="flex-row w-2/6 m-5">
-                    <SearchBar setBookingId={setBookingId} setBookingEmail={setBookingEmail} />
-                    {!!bookingData.length &&
-                        <BookingResult
-                            bookings={bookingData}
-                            setBasket={setBasket}
-                            fetchNextPage={fetchNextPage}
-                            hasNextPage={hasNextPage || false}
-                            isFetchingNextPage={isFetchingNextPage}
-                        />}
+                <div className="flex flex-col w-2/6">
+                    <SearchBar setBookingId={setBookingId} setBookingEmail={setBookingEmail}/>
+                    <div className="flex-row ml-5">
+                        {!!bookings.length &&
+                            <BookingResult
+                                bookings={bookings}
+                                setBookings={setBookings}
+                                setBasket={setBasket}
+                                fetchNextPage={fetchNextPage}
+                                hasNextPage={hasNextPage}
+                                isFetchingNextPage={isFetchingNextPage}
+                            />}
+                    </div>
                 </div>
+
                 <div className="flex-row w-3/12 m-5">
-                    <SaleProducts saleProducts={saleProducts} setBasket={setBasket} setSaleProducts={setSaleProducts} />
+                    <SaleProducts saleProducts={saleProducts} setBasket={setBasket} setSaleProducts={setSaleProducts}/>
                 </div>
                 {basket.length > 0 &&
-                    <div className="flex-row w-3/12 m-5">
-                        <Basket basket={basket} setBasket={setBasket} />
+                    <div className="flex-row w-3/12 mt-5">
+                        <Basket basket={basket} setBasket={setBasket}/>
                     </div>
                 }
             </div>
